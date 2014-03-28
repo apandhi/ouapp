96748c795f10a6058b344d1d9a8a1c8abb24000c
apandhi <ashishpandhi@Ashishs-MacBook-Pro.local>
2014-03-27 22:26:07 -0400

    Login functions fixed

--- app/www/javascripts/login.js
+++ app/www/javascripts/login.js
@@ -1,26 +1,30 @@
 function userLogin(username,password,endpoint)
 {
-var data = {
-   user: username,
-   pass: password,
-   uuid:"0xACA021" //Never changes?
+  var data = {
+     user: username,
+     pass: password,
+     uuid:"0xACA021" //Never changes?
+  }
+
+  $.ajax({ 
+     url: endpoint, 
+     data: data,
+     type: "POST",
+     complete: function(xhr, statusText) { 
+       var response = xhr.responseText;
+
+       if(response&&response.indexOf("Error: Failed Login") == -1){
+           //Logged in
+           alert("Logged in");
+       }else{
+           //Not logged in
+           alert("Failed");
+       }
+       
+     } 
+  }); 
 }
 
-$.ajax({ 
-   url: endpoint, 
-   data: data,
-   type: "POST",
-   complete: function(xhr, statusText) { 
-     var response = xhr.responseText;
-
-     if(response&&response.indexOf("Error: Failed Login") == -1){
-         //Logged in
-         alert("Logged in");
-     }else{
-         //Not logged in
-         alert("Failed");
-     }
-     
-   } 
-}); 
+function login(){
+  userLogin($("#email").val(), $("#password").val(), "https://my.hofstra.edu/cp/home/login?_dc=1395968951166")
 }
