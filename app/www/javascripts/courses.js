5cc90341b13f6d2adcb8696b6bf3eabd78c03018
Vector919 <manbitesdog326@gmail.com>
2014-03-28 04:13:56 -0400

    list align left

--- app/www/javascripts/courses.js
+++ app/www/javascripts/courses.js
@@ -7,6 +7,6 @@ function displayData($obj,data1)
 {
   for(var x =0;x<data1.courses.length;x++)
   {
-    $obj.append('<li class="list-group-item"><span class="badge">0</span>' + data1.courses[x].title + "</li>");
+    $obj.append('<li class="list-group-item" style ="text-align:left"><span class="badge">0</span>' + data1.courses[x].title + "</li>");
   }
 }
