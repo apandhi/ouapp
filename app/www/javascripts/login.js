8703c81c5d7f570d47f0697e97a4b5ed02ddd625
apandhi <ashishpandhi@Ashishs-MacBook-Pro.local>
2014-03-27 21:39:36 -0400

    Working login!

---
+++ app/www/javascripts/login.js
@@ -0,0 +1,22 @@
+var data = {
+   user: username,
+   pass: password,
+   uuid:"0xACA021" //Never changes?
+}
+
+$.ajax({ 
+   url: endpoint, 
+   data: data,
+   type: "POST",
+   complete: function(xhr, statusText) { 
+     var response = xhr.responseText;
+     if(response.indexOf("Error: Failed Login") == -1){
+         //Logged in
+         alert("Logged in");
+     }else{
+         //Not logged in
+         alert("Failed");
+     }
+     
+   } 
+}); 
