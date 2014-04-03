15614cb348ef9f09460bea517f82ff32c97383ed
apandhi <ashishpandhi@ashishs-mbp.dhcp.hofstra.edu>
2014-04-03 18:56:52 -0400

    Native Nav bar fixes

--- app/www/javascripts/courses.js
+++ app/www/javascripts/courses.js
@@ -22,7 +22,6 @@ function displayData($obj,data1)
 $(function(){
     $(".courses-list").html("");
     displayData($(".courses-list"),JSON.parse(localStorage["user_data"]))
-    steroids.view.navigationBar.hide();
 })
 
 var debits = new steroids.views.WebView("mealpoints.html");
@@ -31,3 +30,17 @@ debits.preload();
 function showDebits(){
     steroids.layers.push(debits);
 }
+
+document.addEventListener("visibilitychange", onVisibilityChange, false);
+
+function onVisibilityChange() {
+  setTimeout(function(){
+    if(document.visibilityState == "visible"){
+      steroids.view.navigationBar.hide();
+    }
+  }, 50)
+}
+
+steroids.on('ready', function(){
+  onVisibilityChange();
+});
